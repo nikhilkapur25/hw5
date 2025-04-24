19f9dcdd4ff344f2c2d84d22c2cb092863f25a7f
Nikhil Kapur <nikhilk@usc.edu>
2025-04-24 01:55:39 +0000

    changes

--- schedwork.cpp
+++ schedwork.cpp
@@ -24,6 +24,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add your implementation of schedule() and other helper functions here
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shifts, 
+    size_t day, 
+    size_t slot);
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -36,10 +45,55 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
 
+    for(size_t i = 0; i < numDays; i++) {
+      sched.push_back(vector<Worker_T>(dailyNeed, INVALID_ID));
+    }
 
-
-
+    vector<size_t> shifts(numWorkers, 0); 
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
 }
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shifts, 
+    size_t day, 
+    size_t slot)
+  {
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
+    if(day == numDays) return true;
+
+    if(slot == dailyNeed) return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, day + 1, 0);
+
+    for(Worker_T w = 0; w < numWorkers; w++) {
+      if(avail[day][w] && shifts[w] < maxShifts) {
+        bool alreadyScheduled = false;
+        for(size_t i = 0; i < slot; i++) {
+          if(sched[day][i] == w) {
+            alreadyScheduled = true;
+            break;
+          }
+        }
+
+        if(!alreadyScheduled) {
+          sched[day][slot] = w;
+          shifts[w]++;
+          if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, day, slot + 1)) {
+            return true;
+          }
+
+          sched[day][slot] = INVALID_ID;
+          shifts[w]--;
+        }
+      }
+    }
+    return false;
+  }
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -7,13 +7,19 @@
 #include <set>
 #endif
 
-#include "wordle.h"
+#include "wordle.h" 
 #include "dict-eng.h"
 using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(
+  string& current, 
+  const string& floating, 
+  size_t floatingNeeded, 
+  const set<string>& dict,
+  set<string>& results, 
+  size_t pos);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +27,80 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
+  set<string> results;
+  string current = in;
+
+  wordleHelper(current, floating, floating.length(), dict, results, 0);
+  return results;
     // Add your code here
 
 }
 
+void wordleHelper(
+  string& current, 
+  const string& floating, 
+  size_t floatingNeeded, 
+  const set<string>& dict,
+  set<string>& results, 
+  size_t pos) {
+
+    if (pos == current.length()) {
+      if(dict.find(current) != dict.end() && floatingNeeded == 0) {
+        results.insert(current);
+      }
+      return;
+    }
+
+    if(current[pos] != '-') {
+      bool letterIsFloating = false;
+      for (size_t i = 0; i < floating.length(); i++) {
+        if (current[pos] == floating[i] && floatingNeeded > 0) {
+          letterIsFloating = true;
+          break;
+        }
+      }
+      if (letterIsFloating) {
+        wordleHelper(current, floating, floatingNeeded - 1, dict, results, pos + 1);
+        
+      } else {
+        wordleHelper(current, floating, floatingNeeded, dict, results, pos + 1);
+      }
+      return;
+    }
+  
+    size_t blanksRemaining = 0; 
+    for (size_t i = pos; i < current.length(); i++) {
+      if(current[i] == '-') {
+        blanksRemaining++;
+      }
+    }
+    if (blanksRemaining < floatingNeeded) {
+      return;
+    }
+    for (char c = 'a'; c <= 'z'; c++) {
+      current[pos] = c;
+      bool isFloatingLetter = false; 
+      int floatingIdx = -1;
+
+      for(size_t i = 0; i < floating.length(); i++) {
+        if (c == floating[i]) {
+          isFloatingLetter = true;
+          floatingIdx = i;
+          break;
+        }
+        
+      }
+      if (isFloatingLetter && floatingNeeded > 0) {
+        string newFloating = floating;
+        newFloating.erase(floatingIdx, 1); 
+        wordleHelper(current, newFloating, floatingNeeded - 1, dict, results, pos + 1);
+      }
+      else if (blanksRemaining > floatingNeeded) {
+        wordleHelper(current, floating, floatingNeeded, dict, results, pos + 1);
+      }
+    }
+  current[pos] = '-';
+  }
+
 // Define any helper functions here
+ 
